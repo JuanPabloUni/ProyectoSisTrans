f0188a30f9a8d48c98a91b1cc8f6a9c35f7bc321
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-10-10 10:33:04 -0500

    Update Producto.java

--- src/main/java/uniandes/edu/co/proyecto/modelo/Producto.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Producto.java
@@ -1,5 +1,52 @@
 package uniandes.edu.co.proyecto.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="productos")
 public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String nombre;
+
+    private Float precio;
+
+    public Producto(){;}
+
+    public Producto(String nombre, Float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Float getPrecio(){
+        return precio;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(Float precio){
+        this.precio = precio;
+    }
     
 }
