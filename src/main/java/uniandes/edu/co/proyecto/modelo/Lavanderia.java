6b883041c2e2b20bcb8e6cd87cd98141f037ad66
JuanPabloUni <jp.hernandezt1@uniandes.edu.co>
2023-10-10 09:58:04 -0500

    Updates

--- src/main/java/uniandes/edu/co/proyecto/modelo/Lavanderia.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Lavanderia.java
@@ -1,5 +1,54 @@
 package uniandes.edu.co.proyecto.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="lavanderias")
 public class Lavanderia {
     
+    @Id
+    @OneToOne
+    @JoinColumn(name = "sevicios_id", referencedColumnName = "id")
+    private Servicio servicios_id;
+
+    private String tipoPrenda;
+
+    private Float costoXPrenda;
+
+    public Lavanderia(){;}
+
+    public Lavanderia(Servicio servicios_id, String tipoPrenda, Float costoXPrenda) {
+        this.servicios_id = servicios_id;
+        this.tipoPrenda = tipoPrenda;
+        this.costoXPrenda = costoXPrenda;
+    }
+
+    public Servicio getServicios_id() {
+        return servicios_id;
+    }
+    
+    public void setServicios_id(Servicio servicios_id) {
+        this.servicios_id = servicios_id;
+    }
+
+    public String getTipoPrenda() {
+        return tipoPrenda;
+    }
+
+    public void setTipoPrenda(String tipoPrenda) {
+        this.tipoPrenda = tipoPrenda;
+    }
+
+    public Float getCostoXPrenda() {
+        return costoXPrenda;
+    }
+
+    public void setCostoXPrenda(Float costoXPrenda) {
+        this.costoXPrenda = costoXPrenda;
+    }
+
 }
--- src/main/java/uniandes/edu/co/proyecto/modelo/Prestamo.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Prestamo.java
@@ -1,5 +1,54 @@
 package uniandes.edu.co.proyecto.modelo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="prestamos")
 public class Prestamo {
     
+    @Id
+    @OneToOne
+    @JoinColumn(name = "sevicios_id", referencedColumnName = "id")
+    private Servicio servicios_id;
+
+    private String utensilio;
+
+    private Float costoReemplazo;
+
+    public Prestamo(){;}
+
+    public Prestamo(Servicio servicios_id, String utensilio, Float costoReemplazo) {
+        this.servicios_id = servicios_id;
+        this.utensilio = utensilio;
+        this.costoReemplazo = costoReemplazo;
+    }
+
+    public Servicio getServicios_id() {
+        return servicios_id;
+    }
+    
+    public void setServicios_id(Servicio servicios_id) {
+        this.servicios_id = servicios_id;
+    }
+
+    public String getUtensilio() {
+        return utensilio;
+    }
+
+    public void setUtensilio(String utensilio) {
+        this.utensilio = utensilio;
+    }
+
+    public Float getCostoReemplazo() {
+        return costoReemplazo;
+    }
+
+    public void setCostoReemplazo(Float costoReemplazo) {
+        this.costoReemplazo = costoReemplazo;
+    }
+
 }
